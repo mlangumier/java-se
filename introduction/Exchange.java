9b3435da9dd2d51ee72e6ffdb478600e72e15e00
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-05-22 16:35:37 +0200

    Exercices: introduction

---
+++ introduction/Exchange.java
@@ -0,0 +1,20 @@
+package introduction;
+
+public class Exchange {
+  public static void main(String[] args) {
+      int[] tab = {10, 20, 30};
+      echanger(tab, 0, 2);
+
+      System.out.print("- New values: ");
+      for (int val : tab) {
+          System.out.print(val + ", ");
+      }
+  }
+
+  public static void echanger(int[] tab, int i, int j) {
+    int temp = tab[i];
+    tab[i] = tab[j];
+    tab[j] = temp;
+  }
+
+}
---
+++ introduction/ValeurMax.java
@@ -0,0 +1,29 @@
+package introduction;
+
+public class ValeurMax {
+
+  public static void main(String[] args) {
+    int[] arrayVal = { 38, 5, 65, 18, 15, -2 };
+    int maxValue = getMaxValue(arrayVal);
+    System.out.println("- Valeur max: " + maxValue);
+
+    int[] arrayValNegative = { -50, -5, -65, -18, -15, -2, -7 };
+    int maxValueNegatives = getMaxValue(arrayValNegative);
+    System.out.println("- Valeur max negatives: " + maxValueNegatives);
+  }
+  
+  /**
+   * Récupère le nombre le plus grand d'un tableau
+   * @param valArray Tableau de nombres
+   * @return Le nombre le plus grand du tableau
+   */
+  public static int getMaxValue(int[] valArray) {
+    int maxValue = valArray[0];
+
+    for(int value: valArray) {
+      if (value > maxValue) maxValue = value;
+    }
+
+    return maxValue;
+  }
+}
